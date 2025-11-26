d1a3b071467038777565cd112d6f5735aaef9a5b
carlosheitorinfo <carlosheitor.info@gmail.com>
2025-11-26 02:29:48 -0300

    views

--- CEAP/settings.py
+++ CEAP/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
     'nucleoSaude',
-    
+    'bootstrap5',
    
 ]
 
--- CEAP/urls.py
+++ CEAP/urls.py
@@ -24,6 +24,7 @@ from nucleoSaude import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('nucleoSaude.urls')),
+    path('', views.home, name='home'),
  
 ]
 
